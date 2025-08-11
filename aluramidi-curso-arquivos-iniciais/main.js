03679401957dd6d93873d663cf74436892bea7f7
Igor Tudisco <igortudisco@hotmail.com>
2025-08-11 15:32:05 -0300

    Enhance playSom function with validation and error handling for audio element selection

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,14 @@
-function playSom(idElementAudio) {
-  document.querySelector(idElementAudio).play()
+function playSom(seletorAudio) {
+  if (seletorAudio == '') {
+    console.log('Digite um seletor ou elemento válido')
+  } else {
+    const element = document.querySelector(seletorAudio)
+    if (element && element.localName === 'audio') {
+      element.play()
+    } else {
+      console.log('Elemento ou seletor não encontrado: ' + element?.localName)
+    }
+  }
 }
 
 const listDeTeclas = document.querySelectorAll('.tecla')
@@ -9,5 +18,16 @@ for (let i = 0; i < listDeTeclas.length; i++) {
   const idAudio = `#som_${tecla.classList[1]}`
   tecla.onclick = function () {
     playSom(idAudio)
+  }
+
+  tecla.onkeydown = function (event) {
+    if (event.code === 'Space' || event.code === 'Enter') {
+      tecla.classList.add('ativa')
+    }
+    console.log(event.code)
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa')
   }
 }
